8c3237af0c024fbf0cfa03b8940e2d8bb3406abe
rft0202 <rftester@email.neit.edu>
2023-07-30 13:26:22 -0400

    Skull Stew Part 3

---
+++ Assets/Scripts/FallingScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingScript : MonoBehaviour
+{
+    //Variables go here
+
+    public Vector3 startPos;
+    Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(transform.position.y < -1.5f)
+        {
+            transform.position = startPos;
+            rb.velocity = Vector3.zero; //changes the float values to zero
+        }
+        */
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if(collision.gameObject.tag == "Reset")
+        {
+            transform.position = startPos;
+            rb.velocity = Vector3.zero; //changes the float values to zero
+        }
+    }
+}
